c308ba86fd173513f201b5f64c35bc8c117d0057
Jeffrey Eskes <jeffrey.eskes@gmail.com>
2024-09-02 19:05:15 +0200

    Added/modified HelloWorld component and deleted components that are not used

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,72 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
+<script>
+  export default {
+    name: 'HelloWorld'
   }
-})
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+  <header>
+    <div id="inner-header-wrapper">
+      <img id="logo" src="../assets/RealEstateCare logo.png" alt="logo van RealEstateCare">
+      <div id="h1-wrapper">
+        <h1>Real Estate Care</h1>
+        <p>Caring is in our nature</p>
+      </div>
+    </div>
+    <div class="circle circle-1"></div>
+    <div class="circle circle-2"></div>
+  </header>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+  header {
+    block-size: 130px;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+    position: relative;
   }
-}
+
+  #inner-header-wrapper {
+    inline-size: 80%;
+    margin-inline: auto;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+   #logo {
+    block-size: 80px;
+    inline-size: 80px;
+    margin-inline-end: 30px;
+   }
+
+   h1 {
+    margin-block: 0px;
+   }
+
+   #h1-wrapper p {
+    margin-block: 0px;
+    font-weight: bold;
+   }
+
+   .circle {
+    position: absolute;
+    border-radius: 50%;
+    block-size: 1000px;
+    inline-size: 3000px;
+   }
+
+   .circle-1 {
+    background-color: rgb(0, 170, 162);
+    right: -1500px;
+    top: 0px;
+   }
+   .circle-2 {
+    background: rgb(71,94,108);
+    background: linear-gradient(90deg, rgba(71,94,108,1) 0%, rgba(20,27,31,1) 50%);
+    right: -1700px;
+    top: 40px;
+   }
 </style>
